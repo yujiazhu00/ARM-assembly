1fa07b69dd315b61f104ebe0883cc64d8674a3bf
yujiazhu00 <83460885+yujiazhu00@users.noreply.github.com>
2021-05-01 17:16:25 +0800

    Update multiply.s

--- multiply.s
+++ multiply.s
@@ -1,7 +1,7 @@
 		; Result of multiplication is written to R0
-    ; this code is needed because the VisUAL instruction set does not include multiply
-    ; code multiplies numbers in R0 and R1
-    MOV		R0, #70
+   		; this code is needed because the VisUAL instruction set does not include multiply
+    		; code multiplies numbers in R0 and R1
+    		MOV		R0, #70
 		MOV		R1, #20
 		MOV		R2, #0 ; i
 		MOV		R3, #0 
